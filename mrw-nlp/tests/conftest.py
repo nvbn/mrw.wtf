d9c1e69af99c7ac2587fade4e9e1564698fdb2ce
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-03-08 15:46:20 +0100

    #N/A: Remove pickled after tests

--- mrw-nlp/tests/conftest.py
+++ mrw-nlp/tests/conftest.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 from mrw_nlp import api
 
@@ -5,3 +6,16 @@ from mrw_nlp import api
 @pytest.fixture
 def app():
     return api.app
+
+
+@pytest.fixture(autouse=True)
+def clear_cached(request):
+    def _clear():
+        path = Path(__file__) \
+            .parent \
+            .parent \
+            .joinpath('cached')
+        for cached in path.glob('*.pickle'):
+            cached.unlink()
+
+    request.addfinalizer(_clear)
